cc56f11ba5111fb174ca46ae75dc6893c817b81a
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-04-12 13:45:53 +0700

    Create ListSanPham.java

---
+++ Assignment_JAVA4/src/main/java/viewModel/ListSanPham.java
@@ -0,0 +1,12 @@
+package viewModel;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class ListSanPham {
+    private UUID idProduct;
+    private String tenSP;
+    private Integer soLuong;
+    private BigDecimal giaBan;
+
+}
