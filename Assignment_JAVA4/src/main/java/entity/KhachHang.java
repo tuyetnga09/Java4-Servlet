c8990bdd2db774aa1b4aa0bf24c975f61d9e582f
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-04-12 13:02:41 +0700

    Create KhachHang.java

---
+++ Assignment_JAVA4/src/main/java/entity/KhachHang.java
@@ -0,0 +1,39 @@
+package entity;
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "KhachHang")
+public class KhachHang {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Column(name = "Ma")
+    private String ma;
+    @Column(name = "Ten")
+    private String ten;
+    @Column(name = "TenDem")
+    private String tenDem;
+    @Column(name = "Ho")
+    private String ho;
+    @Column(name = "NgaySinh")
+    private Date ngaySinh;
+    @Column(name = "Sdt")
+    private String sdt;
+    @Column(name = "DiaChi")
+    private String diaChi;
+    @Column(name = "ThanhPho")
+    private String thanhPho;
+    @Column(name = "QuocGia")
+    private String quocGia;
+    @Column(name = "MatKhau")
+    private String matKhau;
+
+
+}
